b6e572b21496697bc0b573130b6c22e5fbfcc551
Aayush Rajvanshi <aayush.rajvanshi@outlook.com>
2020-09-06 18:28:33 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(home: Home()));
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int counter = 1;
+
+  List<String> quotes = [
+    'Officia nulla exercitation incididunt anim nulla magna Lorem officia ipsum aliquip qui sit labore.',
+    'Labore aute occaecat aliqua occaecat.',
+    'Ad aliqua nisi deserunt ex.'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("WhatsApp"),
+        centerTitle: false,
+        backgroundColor: Colors.teal[800],
+        elevation: 0.0,
+      ),
+      backgroundColor: Colors.grey[100],
+      // body: Container(
+      //   child: Center(child: Image.asset("assets/background.png")),
+      // ),
+      body: Container(
+          padding: EdgeInsets.all(40.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: quotes.map((quote) => Text(quote)).toList(),
+          )),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        tooltip: "Send",
+        onPressed: () {
+          setState(() {
+            counter += 1;
+          });
+        },
+        backgroundColor: Colors.teal[800],
+      ),
+    );
+  }
+}
