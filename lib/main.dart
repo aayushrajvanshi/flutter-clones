c466760ca9b80467dad55ceacdf7526707fbeb17
Aayush Rajvanshi <aayush.rajvanshi@outlook.com>
2020-09-08 22:55:03 +0530

    ui changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,201 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:io';
 
-void main() => runApp(MaterialApp(
-      home: Home(),
-    ));
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:path/path.dart' show join;
+import 'package:path_provider/path_provider.dart';
+
+Future<void> main() async {
+  // // Ensure that plugin services are initialized so that `availableCameras()`
+  // // can be called before `runApp()`
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Obtain a list of the available cameras on the device.
+  final cameras = await availableCameras();
+
+  // Get a specific camera from the list of available cameras.
+  final firstCamera = cameras.first;
+
+  runApp(
+    MaterialApp(
+      theme: ThemeData.dark(),
+      home: Home(
+        // Pass the appropriate camera to the TakePictureScreen widget.
+        camera: firstCamera,
+      ),
+    ),
+  );
+}
 
 class Home extends StatefulWidget {
+  final CameraDescription camera;
+
+  const Home({
+    Key key,
+    @required this.camera,
+  }) : super(key: key);
+
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-  int counter = 1;
+  CameraController _controller;
+  Future<void> _initializeControllerFuture;
 
-  List<String> quotes = [
-    'Officia nulla exercitation incididunt anim nulla magna Lorem officia ipsum aliquip qui sit labore.',
-    'Labore aute occaecat aliqua occaecat.',
-    'Ad aliqua nisi deserunt ex.'
-  ];
+  @override
+  void initState() {
+    super.initState();
+    // To display the current output from the Camera,
+    // create a CameraController.
+    _controller = CameraController(
+      // Get a specific camera from the list of available cameras.
+      widget.camera,
+      // Define the resolution to use.
+      ResolutionPreset.medium,
+    );
+
+    // Next, initialize the controller. This returns a Future.
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    // Dispose of the controller when the widget is disposed.
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        backgroundColor: Color.fromRGBO(236, 229, 221, 1),
+        appBar: AppBar(
+          title: Text("WhatsApp"),
+          centerTitle: false,
+          backgroundColor: Color.fromRGBO(7, 94, 84, 1),
+          elevation: 0.0,
+          bottom: TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(Icons.camera_alt),
+              ),
+              Tab(text: "CHATS"),
+              Tab(text: "STATUS"),
+              Tab(text: "CALLS"),
+            ],
+            indicatorColor: Colors.white,
+          ),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {},
+              iconSize: 24,
+              splashRadius: 24,
+            ),
+            IconButton(
+              icon: Icon(Icons.more_vert),
+              onPressed: () {},
+              iconSize: 24,
+              splashRadius: 24,
+            ),
+          ],
+        ),
+        body: TabBarView(
+          children: [
+            FutureBuilder<void>(
+              future: _initializeControllerFuture,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  // If the Future is complete, display the preview.
+                  return CameraPreview(_controller);
+                } else {
+                  // Otherwise, display a loading indicator.
+                  return Center(child: CircularProgressIndicator());
+                }
+              },
+            ),
+            Icon(Icons.chat),
+            Icon(Icons.notifications),
+            Icon(Icons.call),
+          ],
+        ),
+        // body: Container(
+        //     decoration: BoxDecoration(
+        //         image: DecorationImage(
+        //           image: AssetImage("assets/background.png"),
+        //           fit: BoxFit.cover,
+        //         )
+        //     ),
+        //     child: ListView(
+        //       children: <Widget>[
+        //         Column(
+        //           children: <Widget> [
+        //             Container(
+        //                 height: 85,
+        //                 child: Text("hello")
+        //             ),
+        //           ],
+        //         )
+        //       ],
+        //     )
+        // ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.message),
+          onPressed: () async {
+            // Take the Picture in a try / catch block. If anything goes wrong,
+            // catch the error.
+            try {
+              // // Ensure that the camera is initialized.
+              // await _initializeControllerFuture;
+
+              // // Construct the path where the image should be saved using the
+              // // pattern package.
+              // final path = join(
+              //   // Store the picture in the temp directory.
+              //   // Find the temp directory using the `path_provider` plugin.
+              //   (await getTemporaryDirectory()).path,
+              //   '${DateTime.now()}.png',
+              // );
+
+              // // Attempt to take a picture and log where it's been saved.
+              // await _controller.takePicture(path);
+
+              // // If the picture was taken, display it on a new screen.
+              // Navigator.push(
+              //   context,
+              //   MaterialPageRoute(
+              //     builder: (context) => DisplayPictureScreen(imagePath: path),
+              //   ),
+              // );
+            } catch (e) {
+              // If an error occurs, log the error to the console.
+              print(e);
+            }
+          },
+          backgroundColor: Color.fromRGBO(37, 211, 102, 1),
+        ),
+      ),
+    );
+  }
+}
+
+// A widget that displays the picture taken by the user.
+class DisplayPictureScreen extends StatelessWidget {
+  final String imagePath;
+
+  const DisplayPictureScreen({Key key, this.imagePath}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("WhatsApp"),
-        centerTitle: false,
-        backgroundColor: Color.fromRGBO(7, 94, 84, 1),
-        elevation: 0.0,
-      ),
-      backgroundColor: Color.fromRGBO(236, 229, 221, 1),
-      // body: Container(
-      //   child: Center(child: Image.asset("assets/background.png")),
-      // ),
-      body: SafeArea(
-          child: Container(
-              padding: EdgeInsets.all(40.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: quotes.map((quote) => Text(quote)).toList(),
-              ))),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.message),
-        tooltip: "Send",
-        onPressed: () {
-          setState(() {
-            counter += 1;
-          });
-        },
-        backgroundColor: Color.fromRGBO(37, 211, 102, 1),
-      ),
+      appBar: AppBar(title: Text('Display the Picture')),
+      // The image is stored as a file on the device. Use the `Image.file`
+      // constructor with the given path to display the image.
+      body: Image.file(File(imagePath)),
     );
   }
 }
